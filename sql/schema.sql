25de1c2dfef3d3a2584eeab4720c31fb7445eb02
Varvara Yakovleva <yakovleva.varvaraa@gmail.com>
2024-02-13 17:29:49 +0300

    add schema.sql, makefile

---
+++ sql/schema.sql
@@ -0,0 +1,60 @@
+CREATE TABLE task_types (
+    "id" serial NOT NULL,
+	"name" text NOT NULL,
+    "data_types" text[] NOT NULL,
+    "answer_type" text[] NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE tasks (
+    "id" serial NOT NULL,
+	"name" text NOT NULL,
+	"type" int NOT NULL,
+    "data" jsonb NOT NULL,
+	
+	PRIMARY KEY ("id"),
+	FOREIGN KEY ("type") REFERENCES task_types ("id")
+);
+
+CREATE TABLE users (
+    "id" serial NOT NULL,
+	"login" text NOT NULL,
+	"password" text NOT NULL,
+    "email" text NOT NULL,
+	"metadata" jsonb NOT NULL,
+	
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE tests (
+    "id" serial NOT NULL,
+	"user_id" int NOT NULL,
+	"name" text NOT NULL,
+    "description" jsonb NOT NULL,
+	"link" text NOT NULL,
+	
+	PRIMARY KEY ("id"),
+	FOREIGN KEY ("user_id") REFERENCES users ("id")
+);
+
+CREATE TABLE test_task (
+    "id" serial NOT NULL,
+	"variant" int NOT NULL,
+	"test_id" int NOT NULL,
+	"task_id" int NOT NULL,
+    "is_given" bool NOT NULL,
+	
+	PRIMARY KEY ("id"),
+	FOREIGN KEY ("test_id") REFERENCES tests ("id"),
+	FOREIGN KEY ("task_id") REFERENCES tasks ("id")
+);
+
+CREATE TABLE task_result (
+    "id" serial NOT NULL,
+	"test_task_id" int NOT NULL,
+	"answer" jsonb NOT NULL,
+	"data" bytea,
+	
+	PRIMARY KEY ("id"),
+	FOREIGN KEY ("test_task_id") REFERENCES test_task ("id")
+);
