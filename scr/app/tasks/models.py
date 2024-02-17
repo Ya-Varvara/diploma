2deeca97914b78b8f91d4e352778a042ef44308c
Varvara Yakovleva <yakovleva.varvaraa@gmail.com>
2024-02-17 14:29:31 +0300

    Add Task Model

--- scr/app/tasks/models.py
+++ scr/app/tasks/models.py
@@ -1,10 +1,15 @@
-from typing import List
+from typing import List, Any, Dict, TYPE_CHECKING
 
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
-from sqlalchemy import Column, Integer, Text, String, Boolean, JSON, MetaData, ARRAY
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, Integer, Text, String, Boolean, MetaData, ARRAY
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 Base: DeclarativeMeta = declarative_base()
+
+if TYPE_CHECKING:
+    class Task(Base):
+        pass
 
 
 class TaskType(Base):
@@ -14,4 +19,16 @@ class TaskType(Base):
     name: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
     data_types: Mapped[List[str]] = mapped_column(ARRAY(String))
     answer_type: Mapped[List[str]] = mapped_column(ARRAY(String))
+
+    tasks: Mapped[List[Task]] = relationship(back_populates="type")
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
+    type: Mapped[TaskType] = relationship(back_populates="id")
+    data: Mapped[Dict[str, Any]] = mapped_column(JSONB)
+
 
