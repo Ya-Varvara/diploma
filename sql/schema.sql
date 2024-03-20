10a0c6295fee09d2d208992c1c5603507281e4e8
Yakovleva V.A <yakovleva.varvaraa@gmail.com>
2024-03-20 11:42:05 +0300

    change db

--- sql/schema.sql
+++ sql/schema.sql
@@ -18,20 +18,66 @@ INSERT INTO public.users(
 CREATE TABLE task_types (
     "id" serial NOT NULL,
 	"name" text NOT NULL,
-    "data_types" text[] NOT NULL,
-    "answer_type" text[] NOT NULL,
+    "condition_form_type" text NOT NULL unique,
+    "answer_form_type" text NOT NULL unique,
 	"user_id" int NOT NULL,
+	"settings" json,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL,
+	"deleted" bool NOT NULL,
 
 	PRIMARY KEY ("id"),
 	FOREIGN KEY ("user_id") REFERENCES users ("id")
 );
 
+CREATE TABLE forms (
+    "id" serial NOT NULL,
+	"name" text NOT NULL,
+    "short_name" text NOT NULL,
+	"condition_form" bool NOT NULL,
+	"answer_form" bool NOT NULL,
+
+	PRIMARY KEY ("id")
+);
+
+INSERT INTO public.forms (name, short_name, condition_form, answer_form)
+	VALUES 
+	('таблица', 'table', true, false),
+	('ввод', 'input', false, true),
+	('выбор одного варианта', 'radio', false, true),
+	('выбор нескольких вариантов варианта', 'checkbox', false, true),
+	('загрузка файлов', 'upload', false, true);
+
+CREATE TABLE task_types_condition_forms (
+    "task_type_form_id" text NOT NULL,
+	"form_id" int NOT NULL,
+
+	PRIMARY KEY ("task_type_form_id", "form_id"),
+	FOREIGN KEY ("task_type_form_id") REFERENCES task_types ("condition_form_type"),
+	FOREIGN KEY ("form_id") REFERENCES forms ("id")
+);
+
+CREATE TABLE task_types_answer_forms (
+    "task_type_form_id" text NOT NULL,
+	"form_id" int NOT NULL,
+
+	PRIMARY KEY ("task_type_form_id", "form_id"),
+	FOREIGN KEY ("task_type_form_id") REFERENCES task_types ("answer_form_type"),
+	FOREIGN KEY ("form_id") REFERENCES forms ("id")
+);
+
+
 CREATE TABLE tasks (
     "id" serial NOT NULL,
 	"name" text NOT NULL,
 	"type_id" int NOT NULL,
-    "data" jsonb NOT NULL,
+    "description_data" text NOT NULL,
+	"condition_data" jsonb NOT NULL,
+	"answer_data" jsonb NOT NULL,
 	"user_id" int NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL,
+	"deleted" bool NOT NULL,
 	
 	PRIMARY KEY ("id"),
 	FOREIGN KEY ("type_id") REFERENCES task_types ("id"),
@@ -42,11 +88,27 @@ CREATE TABLE tests (
     "id" serial NOT NULL,
 	"name" text NOT NULL,
 	"user_id" int NOT NULL,
-    "description" jsonb NOT NULL,
+	"start_datetime" timestamp NOT NULL,
+	"end_datetime" timestamp NOT NULL,
+	"test_time" time NOT NULL,
+	"variants_number" int NOT NULL,
 	"link" text NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL,
+	"deleted" bool NOT NULL,
 	
 	PRIMARY KEY ("id"),
 	FOREIGN KEY ("user_id") REFERENCES users ("id")
+);
+
+CREATE TABLE test_task_types (
+	"test_id" int NOT NULL,
+	"task_type_id" int NOT NULL,
+    "number" int NOT NULL,
+	
+	PRIMARY KEY ("test_id", "task_type_id"),
+	FOREIGN KEY ("test_id") REFERENCES tests ("id"),
+	FOREIGN KEY ("task_type_id") REFERENCES task_types ("id")
 );
 
 CREATE TABLE test_task (
