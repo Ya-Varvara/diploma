f16309d2a57d41307b60d369290ba56e0fa584b9
VY <yakovleva.varvaraa@gmail.com>
2024-04-09 10:38:57 +0300

    .

--- api/scr/app/core/models/__init__.py
+++ api/scr/app/core/models/__init__.py
@@ -3,23 +3,26 @@ __all__ = (
     "Task",
     "TaskType",
     "Test",
-    "TestTask",
-    "TestTaskResult",
     "User",
     "Form",
     "TestTaskType",
     "TaskTypesAnswerForm",
     "TaskTypesConditionForm",
     "UploadedFile",
+    "Variant",
+    "VariantTask",
+    "VariantsTaskResult",
+    "VariantResultInfo",
 )
 
 from .base_task_type import BaseTaskType
-from .base import Base
 from .form import Form, TaskTypesAnswerForm, TaskTypesConditionForm
-from .task import Task
 from .task_type import TaskType
+from .task import Task
 from .test import Test, TestTaskType
-from .test_task import TestTask
-from .test_task_result import TestTaskResult
 from .uploaded_file import UploadedFile
 from .user import User
+from .variant import Variant
+from .variant_task import VariantTask
+from .variants_task_result import VariantsTaskResult
+from .variant_result_info import VariantResultInfo
